4ecd0efd4b28dc63ca1c21252223166f1a8562e6
tbrumble <tbrumble@yandex.ru>
2020-05-10 17:59:22 +0300

    Rename test method and add assert messages

--- Basket/test_items.py
+++ Basket/test_items.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class TestBasket:
     @pytest.mark.parametrize("link", ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
-    def test_purchases_button_is_exists(self, browser, link):
+    def test_purchases_button_is_exists_and_work(self, browser, link):
         print("\nstart test_button_is_exists..")
         try:
             print("\nopen link: " + link)
@@ -16,7 +16,7 @@ class TestBasket:
             time.sleep(30)
             print("\nget button")
             button = browser.find_element_by_css_selector("#add_to_basket_form > button")
-            assert button is not None
+            assert button is not None, "No add to cart button"
             print("\nbutton click")
             button.click()
             print("\nwait messages")
@@ -25,13 +25,13 @@ class TestBasket:
             )
             print("\nget message element 1")
             div_element1 = browser.find_element_by_css_selector("#messages > div:nth-child(1)")
-            assert div_element1 is not None
+            assert div_element1 is not None, "Add to cart button doesn't show message 1"
             print("\nget message element 2")
             div_element2 = browser.find_element_by_css_selector("#messages > div:nth-child(2)")
-            assert div_element2 is not None
+            assert div_element2 is not None, "Add to cart button doesn't show message 2"
             print("\nget message element 3")
             div_element3 = browser.find_element_by_css_selector("#messages > div:nth-child(3)")
-            assert div_element3 is not None
+            assert div_element3 is not None, "Add to cart button doesn't show message 3"
         finally:
             print("\nend test_button_is_exists..")
             time.sleep(10)
